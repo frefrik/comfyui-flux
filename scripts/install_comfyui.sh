b863607a2948346b35b5350e7dd2727957c70574
Fredrik Haarstad <swift.year6073@zomg.no>
2024-09-19 23:11:35 +0200

    fix 'git describe --tags' bug

--- scripts/install_comfyui.sh
+++ scripts/install_comfyui.sh
@@ -8,13 +8,13 @@ set -euxo pipefail
 
 # ComfyUI
 cd /app
-git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
+git clone --recurse-submodules \
     https://github.com/comfyanonymous/ComfyUI.git \
     || (cd /app/ComfyUI && git pull)
 
 # ComfyUI Manager
 cd /app/ComfyUI/custom_nodes
-git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
+git clone --recurse-submodules \
     https://github.com/ltdrdata/ComfyUI-Manager.git \
     || (cd /app/ComfyUI/custom_nodes/ComfyUI-Manager && git pull)
 
